a979b98ca7d12ce4ccfe400a84c93ca7be983571
Sam Stephen <samueljstephen@gmail.com>
2015-07-09 11:31:14 -0500

    setup quiz with questions - question displays. wip

--- global.js
+++ global.js
@@ -1,3 +1,34 @@
 /**
- * Created by samuelstephen on 7/9/15.
+ * Created by Samuel Stephen and Nick Schetter on 7/9/15.
  */
+
+var displayQuestion = document.getElementById('question');
+
+var questionArray = [
+    {   question: "What is the name of Ned Stark's youngest daughter?",
+        choices: ["(a) Yari", "(b) Arya", "(c) Darya", "(d) Catelyn"],
+        rightAnswer: "b"
+    },
+
+    {   question: "What is the name of the country 'Game of Thrones' takes place in?",
+        choices: ["(a) Westeros", "(b) Westrose", "(c) Braavos", "(d) King's Landing"],
+        rightAnswer: "a"
+    },
+
+    {   question: "Who wields 'Longclaw' out of these characters?",
+        choices: ["(a) Jaime Lannister", "(b) The Mountain", "(c) Sandor Clegane", "(d) Jon Snow"],
+        rightAnswer: "d"
+    },
+
+    {   question: "What is the name of Jon Snow's dire wolf?",
+        choices: ["(a) Ghost", "(b) Summer", "(c) Shaggy Dog", "(d) Nymeria"],
+        rightAnswer: "a"
+    }   ];
+
+
+
+
+for (var i = 0; i < questionArray.length; i++) {
+    displayQuestion.innerText = questionArray[i].question;
+}
+
